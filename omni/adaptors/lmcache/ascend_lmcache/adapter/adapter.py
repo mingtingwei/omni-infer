048a97d4d2486d793dd564244b8b876f9c7e1cfa
SpecterCipher <dlut_maqiang@163.com>
2025-10-25 11:06:02 +0000

    !945 lmcache bugs fix * some flexible add/delete node bugs fix

--- omni/adaptors/lmcache/ascend_lmcache/adapter/adapter.py
+++ omni/adaptors/lmcache/ascend_lmcache/adapter/adapter.py
@@ -13,19 +13,3 @@ _default_ops = ("lmcache.c_ops", "lmcache.integration.vllm.vllm_adapter")
 
 for _ops in _default_ops:
     replace_module_use_ascend_lmcache(_ops)
-
-from lmcache.integration.vllm.vllm_v1_adapter import LMCacheConnectorV1Impl
-from ascend_lmcache.integration.vllm.vllm_v1_adapter import (
-    start_load_kv,
-    __init__,
-    get_load_kv_failure_reqs,
-)
-
-LMCacheConnectorV1Impl.__init__ = __init__
-LMCacheConnectorV1Impl.start_load_kv = start_load_kv
-LMCacheConnectorV1Impl.get_load_kv_failure_reqs = get_load_kv_failure_reqs
-
-from vllm.distributed.kv_transfer.kv_connector.v1.lmcache_connector import LMCacheConnectorV1
-from ascend_lmcache.integration.vllm.lmcache_connector import get_load_kv_failure_reqs
-
-LMCacheConnectorV1.get_load_kv_failure_reqs = get_load_kv_failure_reqs
--- omni/adaptors/lmcache/ascend_lmcache/integration/lmcacache_connector.py
+++
@@ -1,6 +0,0 @@
-# Standard
-from typing import Optional
-
-
-def get_load_kv_failure_reqs(self) -> Optional[set[str]]:
-    return self._lmcache_engine.get_load_kv_failure_reqs()
