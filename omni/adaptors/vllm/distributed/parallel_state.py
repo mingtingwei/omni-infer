6f6b317972638f514a30411089b2606244834906
Nickyi <liuzhengtao6@huawei.com>
2025-10-27 02:49:38 +0000

    !811 [pangu-718B] 单独配置EP通信组HCCL BUFFSIZE * update pstate * Merge branch 'master' into dev_pstate * Merge branch 'master' into dev_pstate * Merge branch 'master' into dev_pstate * update parallel state

--- omni/adaptors/vllm/distributed/parallel_state.py
+++ omni/adaptors/vllm/distributed/parallel_state.py
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional, List, Any
+from typing import Optional, List, Any, Union
 
 import torch
 import torch.distributed
+from torch.distributed import Backend
 from vllm.distributed import GroupCoordinator as GroupCoordinatorGPU
 from vllm.logger import logger
 from vllm.distributed import (
@@ -29,6 +30,7 @@ from vllm.logger import logger
 from vllm.config import get_current_vllm_config
 from omni.models.config_loader.loader import model_extra_config
 import os
+import torch_npu
 
 initialize_model_parallel_default = parallel_state.initialize_model_parallel
 
@@ -46,6 +48,80 @@ def get_npu_device_count():
 
 
 class GroupCoordinator(GroupCoordinatorGPU):
+
+    def __init__(
+        self,
+        group_ranks: list[list[int]],
+        local_rank: int,
+        torch_distributed_backend: Union[str, Backend],
+        use_device_communicator: bool,  # whether to use device communicator
+        use_message_queue_broadcaster: bool = False,
+        group_name: Optional[str] = None,
+    ):
+        group_name = group_name or "anonymous"
+        self.unique_name = parallel_state._get_unique_name(group_name)
+        parallel_state._register_group(self)
+
+        self.rank = torch.distributed.get_rank()
+        self.local_rank = local_rank
+        self.device_group = None
+        self.cpu_group = None
+
+        torch_distributed_options = torch_npu._C._distributed_c10d.ProcessGroupHCCL.Options()
+        if group_name != "world":
+            hccl_buffer_size = os.getenv('HCCL_BASIC_BUFFSIZE', os.getenv('HCCL_BUFFSIZE', 200))
+            torch_distributed_options.hccl_config = {"hccl_buffer_size":int(hccl_buffer_size)}
+
+        for ranks in group_ranks:
+            device_group = torch.distributed.new_group(
+                ranks, backend=torch_distributed_backend, pg_options=torch_distributed_options)
+            # a group with `gloo` backend, to allow direct coordination between
+            # processes through the CPU.
+            cpu_group = torch.distributed.new_group(ranks, backend="gloo")
+            if self.rank in ranks:
+                self.ranks = ranks
+                self.world_size = len(ranks)
+                self.rank_in_group = ranks.index(self.rank)
+                self.device_group = device_group
+                self.cpu_group = cpu_group
+
+        assert self.cpu_group is not None
+        assert self.device_group is not None
+
+        from vllm.platforms import current_platform
+
+        if current_platform.is_cuda_alike():
+            self.device = torch.device(f"cuda:{local_rank}")
+        elif current_platform.is_out_of_tree():
+            self.device = torch.device(
+                f"{current_platform.device_name}:{local_rank}")
+        else:
+            self.device = torch.device("cpu")
+
+        self.use_device_communicator = use_device_communicator
+
+        self.device_communicator = None
+        if use_device_communicator and self.world_size > 1:
+            from vllm.utils import resolve_obj_by_qualname
+            device_comm_cls = resolve_obj_by_qualname(
+                current_platform.get_device_communicator_cls())
+            self.device_communicator = device_comm_cls(
+                cpu_group=self.cpu_group,
+                device=self.device,
+                device_group=self.device_group,
+                unique_name=self.unique_name,
+            )
+
+        from vllm.distributed.device_communicators.shm_broadcast import (
+            MessageQueue)
+        self.mq_broadcaster: Optional[MessageQueue] = None
+        if use_message_queue_broadcaster and self.world_size > 1:
+            self.mq_broadcaster = MessageQueue.create_from_process_group(
+                self.cpu_group, 1 << 22, 6)
+
+        from vllm.platforms import current_platform
+        self.use_custom_op_call = (current_platform.is_cuda_alike()
+                                   or current_platform.is_tpu())
 
     def all_to_all(
         self,
