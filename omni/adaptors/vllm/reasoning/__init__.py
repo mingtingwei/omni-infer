54e70452c1e52c71b084bbf42d366d4588d314f2
jianghua <jianghua13@huawei.com>
2025-11-10 04:14:05 +0000

    !1180 support kimi k2 thinking w4a8 * csc :1. adapt best practice json load; 2.adapt mtp config * support kimi k2 thinking w4a8

--- omni/adaptors/vllm/reasoning/__init__.py
+++ omni/adaptors/vllm/reasoning/__init__.py
@@ -1,6 +1,8 @@
 from vllm import reasoning
 from .pangu_reasoning_parser import PanguReasoningParser
+from .kimi2_thinking_reasoning_parser import Kimi2ThinkingReasoningParser
 
 
 def register_reasoning():
     reasoning.__all__.append("PanguReasoningParser")
+    reasoning.__all__.append("Kimi2ThinkingReasoningParser")
