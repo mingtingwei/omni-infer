751cb47fe6ea4f2f88353fca147fe8d04614ccbb
fangkang_fk <fk1010098686@outlook.com>
2025-12-03 14:03:13 +0000

    !1553 修复开启EMS和APC后服务崩溃问题 * 修复开启EMS和APC后服务崩溃问题

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -45,3 +45,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/support_v1_priority_schedule_for_xiaoy
 git apply --whitespace=nowarn $PATCH_ROOT/gpt_oss_model_init.patch
 git apply --whitespace=nowarn $PATCH_ROOT/openai_harmony_parser.patch
 git apply --whitespace=nowarn $PATCH_ROOT/apply_tool_parser_content.patch
+git apply --whitespace=nowarn $PATCH_ROOT/fix_ems.patch
