0a1570caf38c4cf047e79f063027db2dd82e87be
hyf19981227 <huangyufei8@huawei.com>
2026-01-31 01:51:59 +0000

    !2013 update omni/adaptors/vllm/patches/fix_internal_server_error.patch. * update omni/adaptors/vllm/patches/fix_internal_server_error.patch. * Fix internal server error * update omni/adaptors/vllm/patches/fix_internal_server_error.patch. * fix internal server error * Fix internal server error * Fix internal server error

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -56,3 +56,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/gpt_oss_make_request_by_chat_template.
 git apply --whitespace=nowarn $PATCH_ROOT/openai_harmony_max_tokens_exclude_reasoning.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix-stream-no-id-and-type.patch
 git apply --whitespace=nowarn $PATCH_ROOT/api_server_print_uvicorn_kwargs.patch
+git apply --whitespace=nowarn $PATCH_ROOT/fix_internal_server_error.patch
