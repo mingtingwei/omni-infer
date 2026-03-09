beff5ea15579d80b0b89d52eb4703695e3221629
hyf19981227 <huangyufei8@huawei.com>
2026-03-09 02:25:23 +0000

    !2124 Fix reasoning content to content when max_tokens is too small * Fix reasoning content to content when max_tokens is too small

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -62,3 +62,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/fix_prefill_profiling_bug.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_content_to_reasoning_content_for_nonstream.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_apc_mtp_conflict.patch
 git apply --whitespace=nowarn $PATCH_ROOT/fix_multimtp.patch
+git apply --whitespace=nowarn $PATCH_ROOT/fix_reasoning_content_to_content.patch
